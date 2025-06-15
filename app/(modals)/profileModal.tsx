23bb2adb37690312c06607752a8407c5906242af
mohdshuhaib <mohdshuhaib357@gmail.com>
2025-06-15 15:29:32 +0530

    more addded

--- app/(modals)/profileModal.tsx
+++ app/(modals)/profileModal.tsx
@@ -1,13 +1,118 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { Alert, ScrollView, StyleSheet, Text, Touchable, TouchableOpacity, View } from 'react-native'
+import React, { useEffect, useState } from 'react'
 import { colors, spacingX, spacingY } from '@/constants/theme'
 import { scale, verticalScale } from '@/utils/styling'
+import ModalWrapper from '@/components/ModalWrapper'
+import Header from '@/components/Header'
+import BackButton from '@/components/BackButton'
+import { Image } from 'expo-image'
+import { getProfileImage } from '@/services/imageService'
+import * as Icons from 'phosphor-react-native'
+import Typo from '@/components/Typo'
+import { UserDataType } from '../../types';
+import Input from '@/components/Input'
+import Button from '@/components/Button'
+import { useAuth } from '@/contexts/authContext'
+import { updateUser } from '@/services/userService'
+import { useRouter } from 'expo-router'
+import * as ImagePicker from 'expo-image-picker';
 
 const profileModal = () => {
+  const { user, updateUserData } = useAuth()
+  const [userData, setUserData] = useState<UserDataType>({
+    name: '',
+    image: null
+  })
+
+  const [loading, setLoading] = useState(false)
+  const router = useRouter()
+
+  useEffect(() => {
+    setUserData({
+      name: user?.name || '',
+      image: user?.image || null
+    })
+  }, [user])
+
+  const onPickImage = async () => {
+        let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ['images'],
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 0.5,  
+    });
+
+    if (!result.canceled) {
+      setUserData({...userData, image: result.assets[0]})
+    }
+  };
+
+  const onSubmit = async () => {
+    let { name, image } = userData
+    if(!name.trim()) {
+      Alert.alert('User', 'Please fill all the fields')
+      return
+    }
+
+    setLoading(true)
+    const res = await updateUser(user?.uid as string, userData)
+    setLoading(false)
+    if(res.success){
+      updateUserData(user?.uid as string)
+      router.back()
+    }else {
+      Alert.alert('User', res.msg)
+    }
+  }
+
   return (
-    <View>
-      <Text>profileModal</Text>
-    </View>
+    <ModalWrapper>
+      <View style={styles.container}>
+        <Header
+          title='Update Profile'
+          leftIcon={<BackButton/>}
+          style={{ marginBottom: spacingY._10}}
+        />
+
+        {/* form */}
+        <ScrollView contentContainerStyle={styles.form}>
+          <View style={styles.avatarContainer}>
+            <Image
+              style={styles.avatar}
+              source={getProfileImage(userData.image)}
+              contentFit='cover'
+              transition={100}
+            />
+
+            <TouchableOpacity onPress={onPickImage} style={styles.editIcon}>
+              <Icons.Pencil
+                size={verticalScale(20)}
+                color={colors.neutral800}
+              />
+            </TouchableOpacity>
+          </View>
+
+          <View style={styles.inputContainer}>
+            <Typo color={colors.neutral200}>
+              Name
+            </Typo>
+            <Input
+              placeholder='Name'
+              value={userData.name}
+              onChangeText={(value) => setUserData({ ...userData, name: value })}
+            />
+          </View>
+        </ScrollView>
+      </View>
+
+      <View style={styles.footer}>
+        <Button onPress={onSubmit} loading={loading} style={{ flex: 1 }}>
+          <Typo color={colors.black} fontWeight={'700'}>
+            Update
+          </Typo>
+        </Button>
+      </View>
+    </ModalWrapper>
   )
 }
 
---
+++ services/userService.ts
@@ -0,0 +1,16 @@
+import { doc, updateDoc } from 'firebase/firestore';
+import { ResponseType, UserDataType } from '../types';
+import { firestore } from '@/config/firebase';
+
+export const updateUser = async (
+  uid: string,
+  updatedData: UserDataType
+): Promise<ResponseType> => {
+  try {
+    const userRef = doc(firestore, 'users', uid)
+    await updateDoc(userRef, updatedData)
+    return { success: true, msg: 'User updated successfully' }
+  } catch (error: any) {
+    return { success: false, msg: error?.message}
+  }
+}
